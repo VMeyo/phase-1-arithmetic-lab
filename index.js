f03d7e0c24c98998bae700d3c2791843b9efde17
Victor Meyo <victor.meyo@student.moringaschool.com>
2024-12-14 19:37:59 +0300

    Arithmetic lab complete

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+
+//Start of Multiplication code
+const num1 = 31; 
+const num2 = 2;  
+
+
+const multiply = num1 * num2;
+//End of Multiplication code
+
+//Start of Random Number Generation Code
+const random = Math.floor(Math.random() * 10000) + 1;
+//End if Random Number Generation Code
+
+//Start of modulas code
+const num3 = 29;
+const num4 = 5;
+
+const mod = num3 % num4;
+//End of modulas code
+
+//Start of Maximum code
+const max = Math.max( 11, 5, 9, 20, 7);
+
+//End of Maximum code
